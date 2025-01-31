9d223c0aaaf6eb17cf9b55630e196fbf07fdb733
art-dev-litvin <artem.litvin.dev@gmail.com>
2025-01-31 11:15:05 +0200

    add key prop in Education & change submit button color to green

--- src/components/Education/index.tsx
+++ src/components/Education/index.tsx
@@ -10,8 +10,8 @@ export default function Education({ data }: EducationProps) {
     <div id="education">
       <Title variant="h2">Education</Title>
       <div className="flex flex-col gap-3">
-        {data.map(({ duration, list }) => (
-          <div>
+        {data.map(({ duration, list }, i) => (
+          <div key={`education-${duration}-${i}`}>
             <p className="text-lg mb-2 tracking-wider font-bold">{duration}</p>
             <ul className="flex flex-col gap-2 list-disc ml-5">
               {list.map((item) => (
--- src/components/SubmitButton/index.tsx
+++ src/components/SubmitButton/index.tsx
@@ -9,7 +9,7 @@ export default function SubmitButton({ children, className, ...props }: SubmitBu
     <button
       type="submit"
       className={classNames(
-        "w-full h-11 text-lg uppercase font-semibold  bg-blue-700 text-white rounded-md transition-colors duration-200 hover:bg-blue-600 disabled:opacity-70 disabled:pointer-events-none",
+        "w-full h-11 text-lg uppercase font-semibold bg-green-400 text-white rounded-md transition-colors duration-200 hover:bg-green-500 disabled:opacity-70 disabled:pointer-events-none",
         className
       )}
       {...props}>
