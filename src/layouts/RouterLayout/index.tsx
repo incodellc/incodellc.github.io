49dcfbc4ee24d28059ae51f093acf78973def465
Oleg Meleshko <qu1ze34@gmail.com>
2025-01-31 02:57:57 +0200

    updated router to work as HashRouter + switched on LF

--- src/layouts/RouterLayout/index.tsx
+++ src/layouts/RouterLayout/index.tsx
@@ -1,5 +1,5 @@
 import { useState, useEffect } from "react";
-import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+import { HashRouter as Router, Routes, Route } from "react-router-dom";
 import { loadStripe, Stripe } from "@stripe/stripe-js";
 import CompletePage from "../../components/CompletePage";
 import ProtectedCheckoutPage from "../../components/ProtectedCheckoutPage";
