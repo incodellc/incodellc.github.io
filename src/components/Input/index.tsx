6f277346c7d37915fa8cd13dbb9bfd34aa93e0cf
Oleg Meleshko <qu1ze34@gmail.com>
2025-01-31 03:08:01 +0200

    fixed text color and payment forms container gap

--- src/components/Input/index.tsx
+++ src/components/Input/index.tsx
@@ -14,7 +14,7 @@ export default function Input({ className, ...props }: InputProps) {
       type="text"
       {...props}
       className={classNames(
-        "w-full h-12 border-2 rounded-md px-3 text-lg hover:border-slate-400 focus:outline-slate-400",
+        "w-full h-12 border-2 text-black rounded-md px-3 text-lg hover:border-slate-400 focus:outline-slate-400",
         className
       )}
     />
--- src/pages/Pay/index.tsx
+++ src/pages/Pay/index.tsx
@@ -6,8 +6,8 @@ export interface DeveloperInfoPageProps {
 
 export default function DeveloperInfoPage(props: DeveloperInfoPageProps) {
   return (
-    <>
+    <div className="pt-12 lg:pt-20 basis-auto flex flex-col gap-16">
       <PaymentDetailsForm setClientSecret={props.setClientSecret} />
-    </>
+    </div>
   );
 }
